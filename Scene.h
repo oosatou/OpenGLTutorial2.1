77435e5072acd21c3137311a02ad176e0eae2321
oosatou <nekomaru555@gmail.com>
2019-05-14 14:21:00 +0900

    シーンスタッククラスの定義

--- Scene.h
+++ Scene.h
@@ -5,6 +5,7 @@
 #define SCENE_H_INCLUDED
 #include <memory>
 #include <string>
+#include <vector>
 
 class SceneStack;
 
@@ -41,5 +42,37 @@ private:
 };
 
 using ScenePtr = std::shared_ptr<Scene>;
+
+/**
+* シーン管理クラス.
+*/
+class SceneStack
+{
+public:
+	static SceneStack& Instance();
+
+	void Push(ScenePtr);
+	void Pop();
+	void Replace(ScenePtr);
+	Scene& Current();
+	const Scene& Current() const;
+	size_t Size() const;
+	bool Empty() const;
+
+	void Update(float);
+	void Render();
+
+private:
+	SceneStack();
+	SceneStack(const SceneStack&) = delete;
+	SceneStack& operator=(const SceneStack&) = delete;
+	~SceneStack() = default;
+
+	std::vector<ScenePtr> stack;
+};
+
+
+
+
 
 #endif // SCENE_H_INCLUDED
